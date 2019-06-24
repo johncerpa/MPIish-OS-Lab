1be3f90602ca4f7c8ed1a378cb2dce02e0362304
johncerpa <jjcerpa@uninorte.edu.co>
2019-06-24 11:38:20 -0500

    Done, maybe

--- server.py
+++ server.py
@@ -10,7 +10,6 @@ done = False
 def Main():
   num_slaves = int(input('Number of slaves: '))
   threads = int(input('Threads per slave: '))
-
   slaves_threads = []
 
   s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -43,36 +42,18 @@ def Main():
         thr = threading.Thread(target=slave_thread, args=(slave_socket, slaves_connected))
         thr.start()
         slaves_threads.append(thr)
-
         slaves_connected += 1
 
       if (slaves_connected == num_slaves):
         ready = True
-        slaves_connected += 1 # so it only does this once
+        slaves_connected += 1
 
       if (ready):
-        print('All slaves are connected')
         for i in range(len(inputs_list)): # Some slaves might not have a job
-          
-          # Creating a file for each slave
-          with open(f'input{i}.csv', 'w+') as f: 
-            for j in range(len(inputs_list[i])):
-              f.write(str(inputs_list[i][j]) + ',')
-            # Attach number of threads and slave index
-            f.write(str(threads) + ',' + str(i))
+          thr = threading.Thread(target=send_files, args=(i, inputs_list, threads, slaves))
+          thr.start()
 
-          # Sending file with input numbers to each slave
-          slaves[i].send(b'input')
-          with open(f'input{i}.csv', 'rb') as f:
-            slaves[i].sendfile(f, 0)
-
-          # Send program.py
-          slaves[i].send(b'program')
-          with open('prime.py', 'rb') as f: 
-            slaves[i].sendfile(f, 0)
-          slaves[i].send(b'program_done')
-          
-        ready = False # only do it once
+        ready = False
         print('Sent files to every slave')        
 
   except KeyboardInterrupt:
@@ -80,12 +61,30 @@ def Main():
     for slave in slaves:
       slave.close()
 
+def send_files(i, inputs_list, threads, slaves):
+  with open(f'input{i}.csv', 'w+') as f:
+    f.write('INPUTSTART\n')
+    for j in range(len(inputs_list[i])):
+      f.write(str(inputs_list[i][j]) + ',')
+    f.write(str(threads) + ',' + str(i) + '\nINPUTEND\n')
+
+  with open(f'input{i}.csv', 'rb') as f:
+    slaves[i].sendfile(f, 0)
+  
+  with open('prime.py', 'r+') as f:
+    content = f.read()
+    with open('program.py', 'w+') as p:  
+      p.write('PROGRAMSTART\n' + content + '\nPROGRAMEND')
+
+  with open('program.py', 'rb') as f: 
+    slaves[i].sendfile(f, 0)
+
 def slave_thread(slave_socket, index):
   data = slave_socket.recv(1024)
   complete = data.decode('ascii')
   while data:
     try:
-      slave_socket.settimeout(2) # if no more data is available, stop waiting
+      slave_socket.settimeout(4) # if no more data is available, stop waiting
       data = slave_socket.recv(1024)
       complete += data.decode('ascii')
     except (socket.error, socket.timeout) as e:
@@ -97,23 +96,11 @@ def slave_thread(slave_socket, index):
         sys.exit(1)
 
   semaphore.acquire()
-  f = open(f'results.csv', 'a+')
-  f.write(complete)
-  f.close()
+  with open('results.csv', 'a+') as f:
+    f.write(complete)
   semaphore.release()
+  
   slave_socket.close()
 
 if __name__ == '__main__':
   Main()
-
-
-
-
-
-
-
-
-
-
-
-
